0527a244e01c47bbb472878ef973ef729e6c807d
kuro-creator <74273512+kuro-creator@users.noreply.github.com>
2021-01-17 11:20:05 +0700

    Create worker v4 (Sakura-Theme).js

---
+++ worker/worker v4 (Sakura-Theme).js
@@ -0,0 +1,237 @@
+const html = `<!DOCTYPE html>
+<html lang="en-US">
+<head>
+<title>Sakura Project</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/kuro-creator/website-gratis/sakura/sakura.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/kuro-creator/website-gratis/sakura/Bootstrap.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/kuro-creator/website-gratis/sakura/js/bootstrap.min.js">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/kuro-creator/website-gratis/sakura/js/jquery.min.js">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/kuro-creator/website-gratis/css/back-to-top-grey.css">
+
+<style>
+body.custom-background { background-image: url("https://sakuracircle.com/wp-content/uploads/2020/01/bg.png"); background-position: left top; background-size: auto; background-repeat: repeat; background-attachment: scroll; }
+</style>
+</head>
+
+<body class="home blog custom-background hfeed">
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content">Skip to content</a>
+
+<header id="masthead" class="site-header" role="banner">
+  <nav class="site-navigation main-navigation navbar navbar-expand-md navbar-dark" role="navigation">
+    <a class="navbar-brand js-scroll-trigger" href="#" title="Sakura Project" rel="home">Sakura Project</a>
+       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-navbar-collapse" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+	<span class="navbar-toggler-icon"></span>
+</button>
+
+<!-- .site-navigation -->
+
+<div id="main-navbar-collapse" class="collapse navbar-collapse">
+<ul class="nav navbar-nav"><li itemscope="itemscope" class="nav-item dropdown"><a title="Torrents" href="#" data-toggle="dropdown" class="nav-link" aria-haspopup="true">Home<span class="caret"></span></a>
+<li itemscope="itemscope"><a href="https://shorten.wibudrive.tk/eW5ABd" class="nav-link">Contact</a></li>
+<li itemscope="itemscope"><a href="https://github.com/kuro-creator/website-gratis" class="nav-link" target="_blank">Github</a></li>
+</ul></div></nav>
+		
+</header><!-- #masthead -->
+
+<!-- Back to Top -->
+<button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
+
+<!-- project list -->
+
+<div class="container">
+	<div class="row">
+		<div id="primary" class="content-area col-md-12">
+		<main id="main" class="site-main">
+	<ul class="the-loop">		
+
+<li>
+	<article>
+		<header>
+		<img src="https://cdn.shopify.com/s/files/1/0276/0323/9045/products/ZeroTwo-08-Cap-Uniform-Chibi-Ninecolor_1024x1024.jpg?v=1581707275">
+		</header>
+		<div class="entry-meta">	
+		<h2 class="entry-title"><a href="#" rel="bookmark">Project 1</a></h2>	
+		<div class="meta-data"></div><div class="meta-data"><ul class="post-tags"><p>Description</p></div>
+	</article>
+</li>
+
+<li>
+	<article>
+		<header>
+		<img src="https://cdn.shopify.com/s/files/1/0276/0323/9045/products/ZeroTwo-08-Cap-Uniform-Chibi-Ninecolor_1024x1024.jpg?v=1581707275">
+		</header>
+		<div class="entry-meta">	
+		<h2 class="entry-title"><a href="#" rel="bookmark">Project 2</a></h2>	
+		<div class="meta-data"></div><div class="meta-data"><ul class="post-tags"><p>Description</p></div>
+	</article>
+</li>
+
+<li>
+	<article>
+		<header>
+		<img src="https://cdn.shopify.com/s/files/1/0276/0323/9045/products/ZeroTwo-08-Cap-Uniform-Chibi-Ninecolor_1024x1024.jpg?v=1581707275">
+		</header>
+		<div class="entry-meta">	
+		<h2 class="entry-title"><a href="#" rel="bookmark">Project 3</a></h2>	
+		<div class="meta-data"></div><div class="meta-data"><ul class="post-tags"><p>Description</p></div>
+	</article>
+</li>
+
+<li>
+	<article>
+		<header>
+		<img src="https://cdn.shopify.com/s/files/1/0276/0323/9045/products/ZeroTwo-08-Cap-Uniform-Chibi-Ninecolor_1024x1024.jpg?v=1581707275">
+		</header>
+		<div class="entry-meta">	
+		<h2 class="entry-title"><a href="#" rel="bookmark">Project 4</a></h2>	
+		<div class="meta-data"></div><div class="meta-data"><ul class="post-tags"><p>Description</p></div>
+	</article>
+</li>
+
+<li>
+	<article>
+		<header>
+		<img src="https://cdn.shopify.com/s/files/1/0276/0323/9045/products/ZeroTwo-08-Cap-Uniform-Chibi-Ninecolor_1024x1024.jpg?v=1581707275">
+		</header>
+		<div class="entry-meta">	
+		<h2 class="entry-title"><a href="#" rel="bookmark">Project 5</a></h2>	
+		<div class="meta-data"></div><div class="meta-data"><ul class="post-tags"><p>Description</p></div>
+	</article>
+</li>
+
+<li>
+	<article>
+		<header>
+		<img src="https://cdn.shopify.com/s/files/1/0276/0323/9045/products/ZeroTwo-08-Cap-Uniform-Chibi-Ninecolor_1024x1024.jpg?v=1581707275">
+		</header>
+		<div class="entry-meta">	
+		<h2 class="entry-title"><a href="#" rel="bookmark">Project 6</a></h2>	
+		<div class="meta-data"></div><div class="meta-data"><ul class="post-tags"><p>Description</p></div>
+	</article><!-- #post-## -->
+</li>
+
+<li>
+	<article>
+		<header>
+		<img src="https://cdn.shopify.com/s/files/1/0276/0323/9045/products/ZeroTwo-08-Cap-Uniform-Chibi-Ninecolor_1024x1024.jpg?v=1581707275">
+		</header>
+		<div class="entry-meta">	
+		<h2 class="entry-title"><a href="#" rel="bookmark">Project 7</a></h2>	
+		<div class="meta-data"></div><div class="meta-data"><ul class="post-tags"><p>Description</p></div>
+	</article><!-- #post-## -->
+</li>
+
+<li>
+	<article>
+		<header>
+		<img src="https://cdn.shopify.com/s/files/1/0276/0323/9045/products/ZeroTwo-08-Cap-Uniform-Chibi-Ninecolor_1024x1024.jpg?v=1581707275">
+		</header>
+		<div class="entry-meta">	
+		<h2 class="entry-title"><a href="#" rel="bookmark">Project 8</a></h2>	
+		<div class="meta-data"></div><div class="meta-data"><ul class="post-tags"><p>Description</p></div>
+	</article><!-- #post-## -->
+</li>
+
+<li>
+	<article>
+		<header>
+		<img src="https://cdn.shopify.com/s/files/1/0276/0323/9045/products/ZeroTwo-08-Cap-Uniform-Chibi-Ninecolor_1024x1024.jpg?v=1581707275">
+		</header>
+		<div class="entry-meta">	
+		<h2 class="entry-title"><a href="#" rel="bookmark">Project 9</a></h2>	
+		<div class="meta-data"></div><div class="meta-data"><ul class="post-tags"><p>Description</p></div>
+	</article><!-- #post-## -->
+</li>
+
+<li>
+	<article>
+		<header>
+		<img src="https://cdn.shopify.com/s/files/1/0276/0323/9045/products/ZeroTwo-08-Cap-Uniform-Chibi-Ninecolor_1024x1024.jpg?v=1581707275">
+		</header>
+		<div class="entry-meta">	
+		<h2 class="entry-title"><a href="#" rel="bookmark">Project 10</a></h2>	
+		<div class="meta-data"></div><div class="meta-data"><ul class="post-tags"><p>Description</p></div>
+	</article><!-- #post-## -->
+</li>
+
+<li>
+	<article>
+		<header>
+		<img src="https://cdn.shopify.com/s/files/1/0276/0323/9045/products/ZeroTwo-08-Cap-Uniform-Chibi-Ninecolor_1024x1024.jpg?v=1581707275">
+		</header>
+		<div class="entry-meta">	
+		<h2 class="entry-title"><a href="#" rel="bookmark">Project 11</a></h2>	
+		<div class="meta-data"></div><div class="meta-data"><ul class="post-tags"><p>Description</p></div>
+	</article><!-- #post-## -->
+</li>
+
+<li>
+	<article>
+		<header>
+		<img src="https://cdn.shopify.com/s/files/1/0276/0323/9045/products/ZeroTwo-08-Cap-Uniform-Chibi-Ninecolor_1024x1024.jpg?v=1581707275">
+		</header>
+		<div class="entry-meta">	
+		<h2 class="entry-title"><a href="#" rel="bookmark">Project 12</a></h2>	
+		<div class="meta-data"></div><div class="meta-data"><ul class="post-tags"><p>Description</p></div>
+	</article><!-- #post-## -->
+</li>
+
+
+</main><!-- #main -->
+</div><!-- #primary -->
+
+
+<!-- #footer -->
+
+</section></div>
+</section></aside><!-- #secondary -->
+			</div>
+		</div>
+	</div><!-- #content -->
+
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+	
+		<div class="container-fluid">
+			<div class="row">
+				<div class="site-info col-md-12">
+				<a href="https://cloudflare.com">Proudly powered by Cloudflare</a><span class="sep"> | </span>Theme: Sakura by Rahman.</div>
+			</div>
+		</div>
+	</footer><!-- #colophon -->
+
+<!-- #colophon -->
+<script>
+var mybutton = document.getElementById("myBtn");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+</script>
+
+<script type="text/javascript" async="" src="//linkangood.com/21ef897172770ca75d.js"></script></body></html>`
+
+async function handleRequest(request) {
+  return new Response(html, {
+    headers: {
+      "content-type": "text/html;charset=UTF-8",
+    },
+  })
+}
+
+addEventListener("fetch", event => {
+  return event.respondWith(handleRequest(event.request))
+})
